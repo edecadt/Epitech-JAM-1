0adf9d9b7ae7d2bc36e2e932bbca81a98358f5ac
EnzoDubeaurepaire <enzo.dubearepaire@epitech.eu>
2023-12-04 13:33:22 +0100

    [~] better collisions

--- color_map.py
+++ color_map.py
@@ -15,10 +15,16 @@ class ColorMap:
     def update(self, x_to_move, player):
         if player.y >= 10:
             # collision between player and objects on its right (width -> 100, height -> 100)
-            if x_to_move < 0 and self.image.get_at((-self.map_x + player.x + 40 + player.speed, player.y + 99))[1] > 100:
+            if x_to_move < 0 and (self.image.get_at((-self.map_x + player.x + 40 + player.speed, player.y + 99))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x + 40 + player.speed, player.y))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x + 40 + player.speed, player.y + 33))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x + 40 + player.speed, player.y + 66))[1] > 100):
                 return
             # collision between player and objects on its left (width -> 100, height -> 100)
-            if x_to_move > 0 and self.image.get_at((-self.map_x + player.x - player.speed, player.y + 99))[1] > 100:
+            if x_to_move > 0 and (self.image.get_at((-self.map_x + player.x - player.speed, player.y + 99))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x - player.speed, player.y))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x - player.speed, player.y + 33))[1] > 100 or \
+                    self.image.get_at((-self.map_x + player.x - player.speed, player.y + 66))[1] > 100):
                 return
         if x_to_move == 0:
             return
